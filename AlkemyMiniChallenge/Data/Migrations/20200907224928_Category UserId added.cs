a8cb3e42ce4e7bb0c522fab810a5008b614a1583
javiergespejo <javiergespejo@gmail.com>
2020-09-07 20:10:43 -0300

    Models and Database
    Model has been modified for paginated list functionality
    New migration database has been added

---
+++ AlkemyMiniChallenge/Data/Migrations/20200907224928_Category UserId added.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlkemyMiniChallenge.Data.Migrations
+{
+    public partial class CategoryUserIdadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Category",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_UserId",
+                table: "Category",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Category_AspNetUsers_UserId",
+                table: "Category",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Category_AspNetUsers_UserId",
+                table: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Category_UserId",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Category");
+        }
+    }
+}
